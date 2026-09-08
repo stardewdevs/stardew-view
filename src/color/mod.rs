2e95cadd6ef5a129ff99b4e6d2813a5848bad70d
Frostre_ <n9043395@gmail.com>
2026-09-08 07:23:12 +0200

    Implement Color and Palette structs

---
+++ src/color/mod.rs
@@ -0,0 +1,26 @@
+use glam::Vec4;
+
+#[derive(Clone, Copy, Debug)]
+pub struct Color {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+    pub a: f32,
+}
+
+impl Color {
+    pub const BLACK: Self = Self { r: 0.0, g: 0.0, b: 0.0, a: 1.0 };
+    pub const WHITE: Self = Self { r: 1.0, g: 1.0, b: 1.0, a: 1.0 };
+    pub const GREEN: Self = Self { r: 0.0, g: 1.0, b: 0.0, a: 1.0 };
+
+    pub fn to_vec4(&self) -> Vec4 {
+        Vec4::new(self.r, self.g, self.b, self.a)
+    }
+}
+
+pub struct Palette {
+    pub foreground: Color,
+    pub background: Color,
+    pub cursor: Color,
+    pub selection: Color,
+}
