9dd1a7ed53328442a6927f4a01d1ddcbcccc5d4e
Frostre_ <n9043395@gmail.com>
2026-09-08 07:26:47 +0200

    Add jstring_to_string function in utils.rs

---
+++ src/jni/utils.rs
@@ -0,0 +1,8 @@
+use jni::JNIEnv;
+use jni::objects::JString;
+use std::ffi::CStr;
+
+pub fn jstring_to_string(env: &JNIEnv, jstr: JString) -> String {
+    let c_str = env.get_string(jstr).unwrap();
+    c_str.to_str().unwrap().to_string()
+}
