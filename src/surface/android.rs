e2a8d909128ef7c45f6ee42add3f6648908e738c
Frostre_ <n9043395@gmail.com>
2026-09-08 07:16:01 +0200

    Implement AndroidSurface struct for wgpu

---
+++ src/surface/android.rs
@@ -0,0 +1,23 @@
+use ndk::native_window::NativeWindow;
+use wgpu::Surface;
+
+pub struct AndroidSurface {
+    pub window: NativeWindow,
+    pub surface: Surface,
+    pub width: i32,
+    pub height: i32,
+}
+
+impl AndroidSurface {
+    pub fn new(window: NativeWindow, instance: &wgpu::Instance) -> Self {
+        let surface = unsafe { instance.create_surface(&window).unwrap() };
+        let width = window.width();
+        let height = window.height();
+        Self {
+            window,
+            surface,
+            width,
+            height,
+        }
+    }
+}
