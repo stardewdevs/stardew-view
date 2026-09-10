77da02601cca42d914c7f26057d12789f3e8125c
Frostre_ <n9043395@gmail.com>
2026-09-10 14:54:53 +0200

    Refactor WebGpuBackend to use synchronous new method

--- src/gpu/webgpu/mod.rs
+++ src/gpu/webgpu/mod.rs
@@ -1,16 +1,13 @@
 use wgpu::Backends;
 
-pub struct WebGpuBackend {
-    // Similar to Vulkan but with WebGPU-specific settings
-}
+pub struct WebGpuBackend;
 
 impl WebGpuBackend {
-    pub async fn new() -> Self {
-        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+    pub fn new() -> Self {
+        let _instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
             backends: Backends::BROWSER_WEBGPU,
             ..Default::default()
         });
-        // ...
-        unimplemented!()
+        Self
     }
 }
