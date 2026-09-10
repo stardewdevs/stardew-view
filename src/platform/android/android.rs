3e9408cdb856c1a9ef9185a11a29f4218adf6d0d
Frostre_ <n9043395@gmail.com>
2026-09-10 14:53:27 +0200

    Refactor AndroidPlatform by removing Surface
    Removed Surface from AndroidPlatform struct and its initialization.

--- src/platform/android/android.rs
+++ src/platform/android/android.rs
@@ -1,23 +1,12 @@
+#[cfg(target_os = "android")]
 use ndk::native_window::NativeWindow;
-use wgpu::Surface;
 
+#[cfg(target_os = "android")]
 pub struct AndroidPlatform {
     pub window: NativeWindow,
-    pub surface: Surface,
     pub width: i32,
     pub height: i32,
 }
 
-impl AndroidPlatform {
-    pub fn new(window: NativeWindow, instance: &wgpu::Instance) -> Self {
-        let surface = unsafe { instance.create_surface(&window).unwrap() };
-        let width = window.width();
-        let height = window.height();
-        Self {
-            window,
-            surface,
-            width,
-            height,
-        }
-    }
-}
+#[cfg(not(target_os = "android"))]
+pub struct AndroidPlatform;
